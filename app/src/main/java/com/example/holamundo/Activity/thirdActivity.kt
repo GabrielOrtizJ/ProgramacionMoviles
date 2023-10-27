ff43f7f5e5d6714a3fb9a990dfa9a2252388ed4e
Techie9 <gabrieldavidortizj@gmail.com>
2023-10-27 09:49:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/holamundo/Activity/thirdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.holamundo.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.holamundo.R
+
+class thirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
